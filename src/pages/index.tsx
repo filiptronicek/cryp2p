fd45b20edb20ddd6d0ea176c0c77a3ceb249f7d7
Filip Troníček <filip@gitpod.io>
2022-02-19 14:38:32 +0000

    Hooks and stuff

---
+++ src/pages/index.tsx
@@ -0,0 +1,98 @@
+import type { NextPage } from 'next'
+import { Button, Divider } from 'antd';
+
+import {
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import toast, { Toaster } from 'react-hot-toast';
+
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
+import React, { useCallback } from 'react';
+
+const getTestTokens = async (network: 'testnet' | 'devnet', publicKey: any) => {
+  let connection = new Connection(clusterApiUrl(network));
+
+  toast.promise(new Promise(async (resolve, reject) => {
+    const airdropSignature = await connection.requestAirdrop(
+      publicKey,
+      LAMPORTS_PER_SOL * 1,
+    );
+    const sig = await connection.confirmTransaction(airdropSignature);
+    let account = await connection.getAccountInfo(publicKey);
+    console.log(account);
+    if (sig.value.err) {
+      reject(sig.value.err);
+    }
+    resolve('success')
+
+  }), {
+    loading: 'Sending you tokens',
+    success: 'Tokens sent',
+    error: 'Error while sending test tokens',
+  })
+}
+
+const Home: NextPage = () => {
+
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const onClick = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: Keypair.generate().publicKey,
+        lamports: 1,
+      })
+    );
+
+    const signature = await sendTransaction(transaction, connection);
+
+    await connection.confirmTransaction(signature, 'processed');
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      flexWrap: 'wrap'
+    }}>
+      <div><Toaster /></div>
+
+      <div>
+        {!publicKey && (
+          <span>Please log in first</span>
+        )}
+
+        {publicKey &&
+          <div>
+            <p>Connected account {publicKey.toString()}</p>
+            <br />
+            <Button
+              type="primary"
+              onClick={() => getTestTokens('devnet', publicKey)}
+            >
+              Send to yogi
+            </Button>
+            <Divider orientationMargin={20}></Divider>
+            <Button
+              type="primary"
+              onClick={() => getTestTokens('devnet', publicKey)}
+            >
+              Get tokens
+            </Button>
+          </div>}
+      </div>
+    </div>
+  )
+}
+
+export default Home
