f572566f4f4baf111d2f4446b7d5fac867147ae1
Filip Troníček <filip@gitpod.io>
2022-02-19 16:14:28 +0000

    Fix stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Button, Divider } from 'antd';
+import { Button, Divider, InputNumber } from 'antd';
 
 import {
   clusterApiUrl,
@@ -11,10 +11,10 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction } from '@solana/web3.js';
-import React, { useCallback } from 'react';
+import { SystemProgram, Transaction } from '@solana/web3.js';
+import React, { useCallback, useState } from 'react';
 
-const getTestTokens = async (network: 'testnet' | 'devnet', publicKey: any) => {
+const getTestTokens = async (network: 'testnet' | 'devnet', publicKey: PublicKey) => {
   let connection = new Connection(clusterApiUrl(network));
 
   toast.promise(new Promise(async (resolve, reject) => {
@@ -41,22 +41,26 @@ const Home: NextPage = () => {
 
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
+  const [customAmount, setAmount] = useState(0);
+
+  console.log(customAmount);
 
   const onClick = useCallback(async () => {
     if (!publicKey) throw new WalletNotConnectedError();
 
+    console.log(customAmount);
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: publicKey,
         toPubkey: new PublicKey('2GLjNxR3Gf37PhDrMMa1copXXHvpSmwMbv9Qb94TK9yx'),
-        lamports: 10000_000,
+        lamports: customAmount * LAMPORTS_PER_SOL,
       })
     );
 
     const signature = await sendTransaction(transaction, connection);
 
     await connection.confirmTransaction(signature, 'processed');
-  }, [publicKey, sendTransaction, connection]);
+  }, [publicKey, sendTransaction, connection, customAmount]);
 
   return (
     <div style={{
@@ -77,6 +81,17 @@ const Home: NextPage = () => {
           <div>
             <p>Connected account {publicKey.toString()}</p>
             <br />
+            <InputNumber<number>
+              style={{ width: 200 }}
+              defaultValue={1}
+              min={0}
+              max={10_000_000_000}
+              step={0.1}
+              onChange={(amnt) => {
+                setAmount(amnt);
+              }}
+              stringMode={false}
+            />
             <Button
               type="primary"
               onClick={onClick}
